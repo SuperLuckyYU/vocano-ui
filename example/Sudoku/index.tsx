6df400e6495d6bef51dcccfba2b128c5aa237354
huanglei <huanglei@yidian-inc.com>
2021-09-02 16:01:47 +0800

    feat(add a new component sudoku)

--- example/Sudoku/index.tsx
+++ example/Sudoku/index.tsx
@@ -2,11 +2,12 @@
 import React from 'react';
 import { Sudoku } from 'vocano-ui';
 
+
 export default () => (
     <div>
         <Sudoku className='bang' images={1} waterMark={'https://si1.go2yd.com/get-image/0tPBfE2Pei5'}/>
-        <Sudoku images={2} waterMark={'https://si1.go2yd.com/get-image/0tPBfE2Pei5'}/>
-        <Sudoku images={3} waterMark={'https://si1.go2yd.com/get-image/0tPBfE2Pei5'}/>
+        <Sudoku images={'2'} waterMark={'https://si1.go2yd.com/get-image/0tPBfE2Pei5'}/>
+        <Sudoku imageClick={(iamges,index) => console.log(iamges,index)} images={['https://i1.go2yd.com/image.php?url=0XBLlpdAsV&type=jpeg_360x360', 'https://i1.go2yd.com/image.php?url=0X604KAvRU&type=jpeg_360x360', 'https://i1.go2yd.com/image.php?url=0XBLlp2Buz&type=jpeg_360x360']} waterMark={'https://si1.go2yd.com/get-image/0tPBfE2Pei5'} />
         <Sudoku images={4} waterMark={'https://si1.go2yd.com/get-image/0tPBfE2Pei5'}/>
         <Sudoku images={5} waterMark={'https://si1.go2yd.com/get-image/0tPBfE2Pei5'}/>
         <Sudoku images={6} waterMark={'https://si1.go2yd.com/get-image/0tPBfE2Pei5'}/>
--- src/components/Sudoku/index.tsx
+++ src/components/Sudoku/index.tsx
@@ -1,38 +1,63 @@
 import React, { FC, CSSProperties } from 'react';
 import { classNames, uid } from '../../utils';
 
-export interface SkeletonProps {
+type ImageFn = (images: string[], index: number) => void;
+export interface SudokuProps {
   /** 自定义类名 */
   className?: string;
   /** 自定义样式 */
   style?: CSSProperties;
   /** 图片数量或图片数组 */
-  images?: string | number | any[];
+  images?: string | number | string[];
   /** 九宫格图片水印 */
   waterMark?: string;
+  /** 图片的点击事件 */
+  imageClick?: ImageFn;
 }
+type IimageClickProps = (iamges: string[], index: number) => void;
 
 const componentName = 'sudoku';
 
-const Sudoku: FC<SkeletonProps> = props => {
-  const { className, images, waterMark, ...restProps } = props;
+const Sudoku: FC<SudokuProps> = props => {
+  const { className, images, waterMark, imageClick, ...restProps } = props;
   const classes = classNames(componentName, {
     'images-wrapper': true,
     'images-four': images === 4,
     'image-one': images === 1,
     customClassName: className,
   });
+
+  const imageItemClick: IimageClickProps = (album, index) => {
+    (imageClick as ImageFn)(album, index);
+  };
   const renderImages = () => {
     if (!images) return null;
-    const imageList = Array(images)
-      .fill('')
-      .map(() => (
-        <div key={uid()} className={classNames(componentName, 'image-outer')}>
-          <div className={classNames(componentName, 'image')}>
-            {waterMark && <img alt={waterMark} src={waterMark} />}
-          </div>
+    const imagesFormat = Array.isArray(images)
+      ? images
+      : Array(typeof images === 'string' ? parseInt(images, 10) : images).fill('');
+    const imageList = imagesFormat.map((item: string, index: number) => (
+      <div
+        key={uid()}
+        className={classNames(componentName, 'image-outer')}
+        onClick={() => {
+          imageItemClick(imagesFormat, index);
+        }}
+      >
+        <div className={classNames(componentName, 'image')}>
+          {item ? (
+            <img className={classNames(componentName, 'image-item')} alt={item} src={item} />
+          ) : (
+            waterMark && (
+              <img
+                className={classNames(componentName, 'image-watermark')}
+                alt={waterMark}
+                src={waterMark}
+              />
+            )
+          )}
         </div>
-      ));
+      </div>
+    ));
 
     return (
       <div className={classes} {...restProps}>
@@ -48,6 +73,7 @@ const Sudoku: FC<SkeletonProps> = props => {
 Sudoku.defaultProps = {
   images: 0,
   waterMark: '',
+  imageClick: () => {},
 };
 
 export default Sudoku;
