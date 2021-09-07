be7546b85eb3f656b12b79479a7748301af8b5d7
huanglei <huanglei@yidian-inc.com>
2021-09-07 19:01:40 +0800

    optimize(optimize sudoku layout)

--- example/Sudoku/index.tsx
+++ example/Sudoku/index.tsx
@@ -2,17 +2,25 @@
 import React from 'react';
 import { Sudoku } from 'vocano-ui';
 
+const pic = 'https://i1.go2yd.com/image.php?url=0XBLlpdAsV&type=jpeg_360x360';
+const waterMark = 'https://si1.go2yd.com/get-image/0tPBfE2Pei5';
+
+const fillImage = (number) => {
+    return Array(number).fill(pic);
+}
 
 export default () => (
     <div>
-        <Sudoku className='bang' images={1} waterMark={'https://si1.go2yd.com/get-image/0tPBfE2Pei5'}/>
-        <Sudoku images={'2'} waterMark={'https://si1.go2yd.com/get-image/0tPBfE2Pei5'}/>
-        <Sudoku imageClick={(iamges,index) => console.log(iamges,index)} images={['https://i1.go2yd.com/image.php?url=0XBLlpdAsV&type=jpeg_360x360', 'https://i1.go2yd.com/image.php?url=0X604KAvRU&type=jpeg_360x360', 'https://i1.go2yd.com/image.php?url=0XBLlp2Buz&type=jpeg_360x360']} waterMark={'https://si1.go2yd.com/get-image/0tPBfE2Pei5'} />
-        <Sudoku images={4} waterMark={'https://si1.go2yd.com/get-image/0tPBfE2Pei5'}/>
-        <Sudoku images={5} waterMark={'https://si1.go2yd.com/get-image/0tPBfE2Pei5'}/>
-        <Sudoku images={6} waterMark={'https://si1.go2yd.com/get-image/0tPBfE2Pei5'}/>
-        <Sudoku images={7} waterMark={'https://si1.go2yd.com/get-image/0tPBfE2Pei5'}/>
-        <Sudoku images={8} waterMark={'https://si1.go2yd.com/get-image/0tPBfE2Pei5'}/>
-        <Sudoku images={9} waterMark={'https://si1.go2yd.com/get-image/0tPBfE2Pei5'}/>
+        <Sudoku className='bang' images={1} waterMark={waterMark}/>
+        <Sudoku images={'2'} waterMark={waterMark}/>
+        <Sudoku images={fillImage(3)}  imageClick={(iamges,index) => console.log(iamges,index)}  waterMark={waterMark} />
+        <Sudoku images={4} waterMark={waterMark}/>
+        <Sudoku images={fillImage(5)} waterMark={waterMark}/>
+        <div style={{padding: '30px'}}>
+            <Sudoku images={fillImage(6)} waterMark={waterMark}/>
+        </div>
+        <Sudoku images={fillImage(7)} waterMark={waterMark}/>
+        <Sudoku images={fillImage(8)} waterMark={waterMark}/>
+        <Sudoku images={fillImage(9)} waterMark={waterMark}/>
     </div>
   );
