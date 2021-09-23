c63f0b317d4d050cbf3bcf3564f38e2ca3e39913
郝程宇 <haochengyu@yidian-inc.com>
2021-09-23 18:44:55 +0800

    feat(image-preview): support asynchronous shutdown

--- example/ImagePreview/index.tsx
+++ example/ImagePreview/index.tsx
@@ -4,10 +4,11 @@ import { ImagePreview, Button } from 'vocano-ui';
 
 export default () => {
   const handleClick = () => {
-    ImagePreview({
+    const instance = new ImagePreview({
       images: ['https://si1.go2yd.com/get-image/0tZEF9pNZz2', 'https://si1.go2yd.com/get-image/0tZEIWwvJui'],
       startPosition: 1,
     });
+    instance.show();
   }
 
   return (
--- src/components/ImagePreview/ImagePreview.tsx
+++ src/components/ImagePreview/ImagePreview.tsx
@@ -1,4 +1,5 @@
-import React, { FC, useState } from 'react';
+/* eslint-disable class-methods-use-this */
+import React, { FC, useState, useCallback } from 'react';
 import ReactDOM from 'react-dom';
 import Popup from '../Popup';
 import Swipe from '../Swipe';
@@ -17,87 +18,96 @@ interface ImagePreviewOptions {
 
 interface InternalProps extends ImagePreviewOptions {
   startPosition?: string | number;
+  onClose?: () => void;
 }
 
 const componentName = 'image-preview';
+const wrapperEl = document.createElement('div');
 
-const imagePreview = (
-  images: string[] | ImagePreviewOptions,
-  startPosition: string | number = 0,
-) => {
-  const options = Array.isArray(images) ? { images, startPosition } : images;
-  const imagePreviewEl = document.createElement('div');
-  // bad case: compatible doc mobile layout
-  const isDocBrowser = document.querySelector('.__dumi-default-mobile-demo-layout');
-  if (!isDocBrowser) {
-    document.body.appendChild(imagePreviewEl);
-  } else {
-    isDocBrowser.appendChild(imagePreviewEl);
-  }
+const ImagePreviewWrapper: FC<InternalProps> = props => {
+  const popupClasses = classNames(componentName, 'popup');
+  const swipreClasses = classNames(componentName, 'swipe');
+  const swipreItemClasses = classNames(componentName, 'swipe-item');
+  const imgClasses = classNames(componentName, 'img');
+  const previewImageClasses = classNames(componentName, 'image');
+  const previewIndexClasses = classNames(componentName, 'index');
+  const {
+    startPosition: initialSwipe,
+    images: imageList,
+    showIndex = true,
+    swipeDuration = '300',
+    onClose = () => {},
+  } = props;
 
-  document.body.setAttribute('style', 'position: fixed');
+  const [currentIndex, setCurrentIndex] = useState(1);
 
-  const ImagePreview: FC<InternalProps> = props => {
-    const popupClasses = classNames(componentName, 'popup');
-    const swipreClasses = classNames(componentName, 'swipe');
-    const swipreItemClasses = classNames(componentName, 'swipe-item');
-    const imgClasses = classNames(componentName, 'img');
-    const previewImageClasses = classNames(componentName, 'image');
-    const previewIndexClasses = classNames(componentName, 'index');
-    const {
-      startPosition: initialSwipe,
-      images: imageList,
-      showIndex = true,
-      swipeDuration = '300',
-    } = props;
-    const [visible, setVisible] = useState(true);
-    const [currentIndex, setCurrentIndex] = useState(1);
+  const handleChange = useCallback((index: number) => {
+    setCurrentIndex(index + 1);
+  }, []);
 
-    const handleClick = () => {
-      setVisible(false);
-      if (!isDocBrowser) {
-        document.body.removeChild(imagePreviewEl);
-      } else {
-        isDocBrowser.removeChild(imagePreviewEl);
-      }
-      document.body.removeAttribute('style');
-    };
-
-    const handleChange = (index: number) => {
-      setCurrentIndex(index + 1);
-    };
-
-    return (
-      <Popup
-        className={popupClasses}
-        visible={visible}
-        maskStyle="background-color: rgba(0, 0, 0, 0.9) !important; opacity: 1 !important;"
+  return (
+    <Popup
+      className={popupClasses}
+      visible
+      maskStyle="background-color: rgba(0, 0, 0, 0.9) !important; opacity: 1 !important;"
+    >
+      <Swipe
+        className={swipreClasses}
+        initialSwipe={initialSwipe as string | number}
+        showIndicators={false}
+        duration={swipeDuration}
+        handleClickCallback={onClose}
+        onChange={handleChange}
       >
-        <Swipe
-          className={swipreClasses}
-          initialSwipe={initialSwipe as string | number}
-          showIndicators={false}
-          duration={swipeDuration}
-          handleClickCallback={handleClick}
-          onChange={handleChange}
-        >
-          {imageList.map(item => (
-            <Swipe.Item className={swipreItemClasses} key={uid()}>
-              <div className={previewImageClasses}>
-                <img className={imgClasses} src={item} alt={item} />
-              </div>
-            </Swipe.Item>
-          ))}
-        </Swipe>
-        {imageList.length && showIndex && (
-          <div className={previewIndexClasses}>
-            {currentIndex} / {imageList.length}
-          </div>
-        )}
-      </Popup>
-    );
+        {imageList.map(item => (
+          <Swipe.Item className={swipreItemClasses} key={uid()}>
+            <div className={previewImageClasses}>
+              <img className={imgClasses} src={item} alt={item} />
+            </div>
+          </Swipe.Item>
+        ))}
+      </Swipe>
+      {imageList.length && showIndex && (
+        <div className={previewIndexClasses}>
+          {currentIndex} / {imageList.length}
+        </div>
+      )}
+    </Popup>
+  );
+};
+
+class ImagePreview {
+  private options: InternalProps = {
+    images: [],
   };
 
-  ReactDOM.render(<ImagePreview {...options} />, imagePreviewEl);
-};
-export default imagePreview;
+  private isDocBrowser = document.querySelector('.__dumi-default-mobile-demo-layout');
+
+  constructor(
+    images: string[] | ImagePreviewOptions,
+    startPosition: string | number = 0,
+  ) {
+    this.options = Array.isArray(images) ? { images, startPosition } : images;
+  };
+
+  show () {
+    // bad case: compatible doc mobile layout
+    if (!this.isDocBrowser) {
+      document.body.appendChild(wrapperEl);
+    } else {
+      this.isDocBrowser.appendChild(wrapperEl);
+    }
+
+    document.body.setAttribute('style', 'position: fixed');
+    ReactDOM.render(<ImagePreviewWrapper {...this.options} onClose={this.close} />, wrapperEl);
+  };
+
+  close () {
+    ReactDOM.unmountComponentAtNode(wrapperEl);
+    const maskEl = document.getElementById('vo-mask');
+    if(maskEl) document.body.removeChild(maskEl);
+    document.body.removeAttribute('style');
+  };
+}
+
+export default ImagePreview;
